1e12d48b45c98983ced61737e50cd3dde3f51ea3
Sergey A. Tachenov <sergey@tachenov.name>
2016-04-07 21:17:25 +0300

    Longest string with at most k distinct characters.

---
+++ src/name/tachenov/leetcode/LongestAtMostK.java
@@ -0,0 +1,15 @@
+/*
+ * (C) Sergey A. Tachenov
+ * This thing belongs to public domain. Really.
+ */
+package name.tachenov.leetcode;
+
+/**
+ *
+ * @author Sergey A. Tachenov
+ */
+public class LongestAtMostK {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        return 0;
+    }
+}
