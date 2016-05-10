570315bf1fb1de9b013f83653b43f61927263d30
Sergey A. Tachenov <sergey@tachenov.name>
2016-05-10 14:30:57 +0300

    O(1) TicTacToe

--- src/main/java/name/tachenov/leetcode/TicTacToe.java
+++ src/main/java/name/tachenov/leetcode/TicTacToe.java
@@ -4,8 +4,6 @@
  */
 package name.tachenov.leetcode;
 
-import java.util.stream.IntStream;
-
 /**
  * TicTacToe (problem #348).
  * @author Sergey A. Tachenov
@@ -14,13 +12,19 @@ public class TicTacToe {
     
     public static final int X = 1;
     public static final int O = 2;
-    private final int[][] field;
+    private final int[][] rowMoveCount;
+    private final int[][] columnMoveCount;
+    private final int[] mainDiagonalMoveCount;
+    private final int[] counterDiagonalMoveCount;
 
     /** Initialize your data structure here.
      * @param n size of the field
      */
     public TicTacToe(int n) {
-        field = new int[n][n];
+        rowMoveCount = new int[2][n];
+        columnMoveCount = new int[2][n];
+        mainDiagonalMoveCount = new int[2];
+        counterDiagonalMoveCount = new int[2];
     }
     
     /** Player {player} makes a move at ({row}, {col}).
@@ -32,21 +36,24 @@ public class TicTacToe {
                 1: Player 1 wins.
                 2: Player 2 wins. */
     public int move(int row, int col, int player) {
-        field[row][col] = player;
-        if (IntStream.range(0, field.length).map(i -> field[i][col]).allMatch(f -> f == player)
-                || IntStream.range(0, field.length).map(j -> field[row][j]).allMatch(f -> f == player)) {
+        ++rowMoveCount[player - 1][row];
+        ++columnMoveCount[player - 1][col];
+        if (row == col) {
+            ++mainDiagonalMoveCount[player - 1];
+        }
+        if (row == getFieldSize() - 1 - col) {
+            ++counterDiagonalMoveCount[player - 1];
+        }
+        if (rowMoveCount[player - 1][row] == getFieldSize()
+                || columnMoveCount[player - 1][col] == getFieldSize()
+                || mainDiagonalMoveCount[player - 1] == getFieldSize()
+                || counterDiagonalMoveCount[player - 1] == getFieldSize()) {
             return player;
         }
-        if (row == col) { // main diagonal
-            if (IntStream.range(0, field.length).map(i -> field[i][i]).allMatch(f -> f == player)) {
-                return player;
-            }
-        }
-        if (row == field.length - 1 - col) { // counter diagonal
-            if (IntStream.range(0, field.length).map(i -> field[i][field.length - 1 - i]).allMatch(f -> f == player)) {
-                return player;
-            }
-        }
         return 0;
+    }
+    
+    private int getFieldSize() {
+        return rowMoveCount[0].length;
     }
 }
