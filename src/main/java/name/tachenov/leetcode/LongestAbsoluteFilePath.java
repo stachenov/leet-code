9075232b9ead992b9ed069db26736375e3db4553
Sergey A. Tachenov <sergey@tachenov.name>
2016-08-26 21:30:59 +0300

    Clean code

--- src/main/java/name/tachenov/leetcode/LongestAbsoluteFilePath.java
+++ src/main/java/name/tachenov/leetcode/LongestAbsoluteFilePath.java
@@ -9,29 +9,60 @@ import java.util.*;
 public class LongestAbsoluteFilePath {
 
     public int lengthLongestPath(String input) {
-        Deque<String> currentDir = new ArrayDeque<>();
-        int len = 0;
+        Path currentDir = new Path();
         int longest = 0;
-        String[] lines = input.split("\n");
-        for (String line : lines) {
-            int depth = 1;
-            for (char c : line.toCharArray()) {
-                if (c != '\t')
-                    break;
-                ++depth;
-            }
-            while (depth <= currentDir.size()) {
-                len -= currentDir.pop().length();
-            }
-            boolean dir = !line.contains(".");
-            if (dir) {
-                String dname = line.substring(depth - 1);
-                currentDir.push(dname);
-                len += dname.length();
-            } else {
-                longest = Math.max(longest, len + currentDir.size() + line.length() - (depth - 1));
-            }
+        for (String line : input.split("\n")) {
+            int tabsCount = countLeadingTabs(line);
+            int depth = tabsCount + 1;
+            currentDir.cdUpToDepth(depth - 1);
+            String localName = line.substring(tabsCount);
+            boolean dir = !localName.contains(".");
+            if (dir)
+                currentDir.cd(localName);
+            else
+                longest = Math.max(longest, currentDir.length + localName.length());
         }
         return longest;
+    }
+
+    private int countLeadingTabs(String line) {
+        for (int i = 0; i < line.length(); ++i) {
+            if (line.charAt(i) != '\t')
+                return i;
+        }
+        return line.length();
+    }
+    
+    private static class Path {
+        private static final int SEPARATOR_LENGTH = 1;
+        private final Deque<String> stack = new ArrayDeque<>();
+        private int length = 0;
+        
+        void cd(String dir) {
+            stack.push(dir);
+            addLength(dir);
+        }
+
+        private void addLength(String dir) {
+            length += dir.length() + SEPARATOR_LENGTH;
+        }
+
+        private void cdUpToDepth(int target) {
+            while (depth() > target)
+                cdUp();
+        }
+
+        private int depth() {
+            return stack.size();
+        }
+
+        private void cdUp() {
+            String dirName = stack.pop();
+            subtractLength(dirName);
+        }
+
+        private void subtractLength(String dirName) {
+            length -= dirName.length() + SEPARATOR_LENGTH;
+        }
     }
 }
