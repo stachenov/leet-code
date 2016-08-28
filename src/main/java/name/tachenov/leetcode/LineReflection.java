13299748e04d57a12a9e45706ab481699f1f495a
Sergey A. Tachenov <sergey@tachenov.name>
2016-08-28 17:16:24 +0300

    O(1)

--- src/main/java/name/tachenov/leetcode/LineReflection.java
+++ src/main/java/name/tachenov/leetcode/LineReflection.java
@@ -4,77 +4,35 @@
  */
 package name.tachenov.leetcode;
 
+import static java.lang.Math.*;
 import java.util.*;
 
 public class LineReflection {
-    private static final Comparator<Point> X_ASC
-            = (p1, p2) -> Integer.compare(p1.x, p2.x);
-    private static final Comparator<Point> X_ASC_Y_ASC
-            = (p1, p2) -> {
-        int cmpx = Integer.compare(p1.x, p2.x);
-        if (cmpx == 0)
-            return Integer.compare(p1.y, p2.y);
-        else
-            return cmpx;
-    };
-    private static final Comparator<Point> X_DESC_Y_ASC
-            = (p1, p2) -> {
-        int cmpx = Integer.compare(p2.x, p1.x);
-        if (cmpx == 0)
-            return Integer.compare(p1.y, p2.y);
-        else
-            return cmpx;
-    };
 
     public boolean isReflected(int[][] points) {
         if (points.length == 0)
             return true;
-        List<Point> uniquePoints = getUniquePoints(points);
-        Collections.sort(uniquePoints, X_ASC);
+        Set<Point> uniquePoints = getUniquePoints(points);
         ReflectionLine line = findReflectionLine(uniquePoints);
-        List<Point> pointsToTheLeft = pointsToTheLeft(uniquePoints);
-        List<Point> pointsToTheRight = pointsToTheRight(uniquePoints);
-        assert pointsToTheLeft.size() == pointsToTheRight.size();
-        Collections.sort(pointsToTheLeft, X_ASC_Y_ASC);
-        Collections.sort(pointsToTheRight, X_DESC_Y_ASC);
-        for (int i = 0; i < pointsToTheLeft.size(); ++i) {
-            if (!line.reflects(pointsToTheLeft.get(i), pointsToTheRight.get(i)))
-                return false;
-        }
-        return true;
+        return line.allPointsHaveReflection(uniquePoints);
     }
 
-    private List<Point> getUniquePoints(int[][] points) {
-        List<Point> uniquePoints = new ArrayList<>();
-        Set<Point> added = new HashSet<>();
+    private Set<Point> getUniquePoints(int[][] points) {
+        Set<Point> uniquePoints = new HashSet<>();
         for (int[] p : points) {
             Point point = new Point(p[0], p[1]);
-            if (added.add(point))
-                uniquePoints.add(point);
+            uniquePoints.add(point);
         }
         return uniquePoints;
     }
 
-    private ReflectionLine findReflectionLine(List<Point> uniquePoints) {
-        int mid = uniquePoints.size() / 2;
-        if (uniquePoints.size() % 2 == 0) {
-            int x1 = uniquePoints.get(mid - 1).x;
-            int x2 = uniquePoints.get(mid).x;
-            return new ReflectionLine(x1, x2);
-        } else {
-            return new ReflectionLine(uniquePoints.get(mid).x);
+    private ReflectionLine findReflectionLine(Set<Point> uniquePoints) {
+        int minX = Integer.MAX_VALUE, maxX = Integer.MIN_VALUE;
+        for (Point p : uniquePoints) {
+            minX = min(minX, p.x);
+            maxX = max(maxX, p.x);
         }
-    }
-
-    private List<Point> pointsToTheLeft(List<Point> uniquePoints) {
-        return uniquePoints.subList(0, uniquePoints.size() / 2);
-    }
-
-    private List<Point> pointsToTheRight(List<Point> uniquePoints) {
-        if ((uniquePoints.size() & 1) == 1)
-            return uniquePoints.subList(uniquePoints.size() / 2 + 1, uniquePoints.size());
-        else
-            return uniquePoints.subList(uniquePoints.size() / 2, uniquePoints.size());
+        return new ReflectionLine(minX, maxX);
     }
     
     private static class ReflectionLine {
@@ -86,24 +44,33 @@ public class LineReflection {
             this.andAHalf = ((x1 + x2) & 1) != 0;
         }
 
-        ReflectionLine(int x) {
-            this.x = x;
-            this.andAHalf = false;
+        boolean allPointsHaveReflection(Set<Point> uniquePoints) {
+            for (Point p : uniquePoints) {
+                Point r = reflect(p);
+                if (!uniquePoints.contains(r))
+                    return false;
+            }
+            return true;
         }
 
-        boolean reflects(Point p1, Point p2) {
-            if (bothPointsOnTheLine(p1, p2))
-                return true;
-            if (p1.y != p2.y)
-                return false;
-            if (andAHalf)
-                return p2.x - x == x - p1.x + 1;
-            else
-                return p2.x - x == x - p1.x;
+        private Point reflect(Point p) {
+            if (onTheLine(p))
+                return p;
+            if (andAHalf) {
+                if (p.x < x)
+                    return new Point(x + (x - p.x) + 1, p.y);
+                else
+                    return new Point(x - (p.x - x - 1), p.y);
+            } else {
+                if (p.x < x)
+                    return new Point(x + (x - p.x), p.y);
+                else
+                    return new Point(x - (p.x - x), p.y);
+            }
         }
 
-        private boolean bothPointsOnTheLine(Point p1, Point p2) {
-            return !andAHalf && p1.x == x && p2.x == x;
+        private boolean onTheLine(Point p) {
+            return !andAHalf && p.x == x;
         }
     }
     
