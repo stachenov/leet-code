3e8466f3ab0dce631bd93bc5880a2aaee9f3b22b
Sergey A. Tachenov <sergey@tachenov.name>
2016-08-28 10:37:52 +0300

    Cleaner code

--- src/main/java/name/tachenov/leetcode/PerfectRectangle.java
+++ src/main/java/name/tachenov/leetcode/PerfectRectangle.java
@@ -5,45 +5,53 @@
 package name.tachenov.leetcode;
 
 import java.util.*;
+import java.util.stream.*;
 
 public class PerfectRectangle {
     public boolean isRectangleCover(int[][] rectangles) {
-        int[] bounds = new int[4];
-        int left = Integer.MAX_VALUE, right = Integer.MIN_VALUE,
-                top = Integer.MIN_VALUE, bottom = Integer.MAX_VALUE;
+        int totalArea = Stream.of(rectangles).mapToInt(r -> area(r)).sum();
+        if (totalArea != area(getBounds(rectangles)))
+            return false;
         List<KeyPoint> keyPointsX = new ArrayList<>();
-        int totalArea = 0;
         for (int i = 0; i < rectangles.length; ++i) {
-            left = Math.min(left, rectangles[i][0]);
-            right = Math.max(right, rectangles[i][2]);
-            top = Math.max(top, rectangles[i][3]);
-            bottom = Math.min(bottom, rectangles[i][1]);
-            totalArea += area(rectangles[i]);
             keyPointsX.add(new KeyPoint(rectangles[i][0], rectangles[i], true));
             keyPointsX.add(new KeyPoint(rectangles[i][2], rectangles[i], true));
         }
-        bounds[0] = left;
-        bounds[1] = bottom;
-        bounds[2] = right;
-        bounds[3] = top;
         Collections.sort(keyPointsX);
         KeyPointList keyPointsY = new KeyPointList();
         for (KeyPoint kpx : keyPointsX) {
+            KeyPoint start = new KeyPoint(kpx.rectangle[1], kpx.rectangle, false);
+            KeyPoint end = new KeyPoint(kpx.rectangle[3], kpx.rectangle, false);
             if (kpx.isRectangleEndsHere()) {
-                keyPointsY.remove(new KeyPoint(kpx.rectangle[1], kpx.rectangle, false));
-                keyPointsY.remove(new KeyPoint(kpx.rectangle[3], kpx.rectangle, false));
+                keyPointsY.remove(start);
+                keyPointsY.remove(end);
             } else {
-                KeyPoint start = new KeyPoint(kpx.rectangle[1], kpx.rectangle, false);
-                KeyPoint end = new KeyPoint(kpx.rectangle[3], kpx.rectangle, false);
                 int insertionPoint = keyPointsY.getNonOverlappingInsertionPoint(start, end);
                 if (insertionPoint == -1)
                     return false;
                 keyPointsY.add(insertionPoint, start, end);
             }
         }
-        return totalArea == area(bounds);
+        return true;
     }
 
+    private int[] getBounds(int[][] rectangles) {
+        int[] bounds = new int[4];
+        int left = Integer.MAX_VALUE, right = Integer.MIN_VALUE,
+                top = Integer.MIN_VALUE, bottom = Integer.MAX_VALUE;
+        for (int i = 0; i < rectangles.length; ++i) {
+            left = Math.min(left, rectangles[i][0]);
+            right = Math.max(right, rectangles[i][2]);
+            top = Math.max(top, rectangles[i][3]);
+            bottom = Math.min(bottom, rectangles[i][1]);
+        }
+        bounds[0] = left;
+        bounds[1] = bottom;
+        bounds[2] = right;
+        bounds[3] = top;
+        return bounds;
+    }
+    
     private static int area(int[] rectangle) {
         return (rectangle[2] - rectangle[0]) * (rectangle[3] - rectangle[1]);
     }
@@ -52,7 +60,7 @@ public class PerfectRectangle {
         final List<KeyPoint> keyPoints = new ArrayList<>();
 
         void remove(KeyPoint keyPoint) {
-            keyPoints.remove(keyPoint);
+            keyPoints.remove(Collections.binarySearch(keyPoints, keyPoint));
         }
 
         int getNonOverlappingInsertionPoint(KeyPoint start, KeyPoint end) {
@@ -107,35 +115,15 @@ public class PerfectRectangle {
         public int compareTo(KeyPoint o) {
             int xc = Integer.compare(coordinate, o.coordinate);
             if (xc == 0) {
-                if (isRectangleEndsHere() && !o.isRectangleEndsHere())
+                if (isRectangleEndsHere() && o.isRectangleStartsHere())
                     return -1;
-                else if (!isRectangleEndsHere() && o.isRectangleEndsHere())
+                else if (isRectangleStartsHere() && o.isRectangleEndsHere())
                     return +1;
                 else
                     return xc;
             } else {
                 return xc;
             }
-        }
-
-        @Override
-        public int hashCode() {
-            int hash = 7;
-            hash = 79 * hash + this.coordinate;
-            hash = 79 * hash + this.rectangle.hashCode();
-            return hash;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-            final KeyPoint other = (KeyPoint) obj;
-            return this.coordinate == other.coordinate
-                && this.rectangle == other.rectangle
-                && this.horizontal == other.horizontal;
         }
     }
 }
