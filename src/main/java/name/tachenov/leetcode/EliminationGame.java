26284340f01505c3f68bd3a241fe91b5e9219cc5
Sergey A. Tachenov <sergey@tachenov.name>
2016-08-30 20:25:19 +0300

    The awesome Stephan Pochmann's solution

--- src/main/java/name/tachenov/leetcode/EliminationGame.java
+++ src/main/java/name/tachenov/leetcode/EliminationGame.java
@@ -4,25 +4,8 @@
  */
 package name.tachenov.leetcode;
 
-import java.util.*;
-
-/**
- *
- * @author Sergey A. Tachenov
- */
 public class EliminationGame {
     public int lastRemaining(int n) {
-        int a = 1, b = 1; // 1k + 1
-        int m = n;
-        boolean ltr = true;
-        while (m > 1) {
-            if (ltr || (m & 1) == 1) {
-                b += a;
-            }
-            a *= 2;
-            m /= 2;
-            ltr = !ltr;
-        }
-        return b;
+        return 1 + ((Integer.highestOneBit(n) - 1) & (n | 0x55555555));
     }
 }
