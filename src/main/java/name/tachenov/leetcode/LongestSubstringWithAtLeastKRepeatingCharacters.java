a88b73dfd23a2c5e9a84c1b07ac10a48aa311656
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-04 22:03:57 +0300

    The intervals solution

--- src/main/java/name/tachenov/leetcode/LongestSubstringWithAtLeastKRepeatingCharacters.java
+++ src/main/java/name/tachenov/leetcode/LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -8,38 +8,56 @@ import java.util.*;
 
 public class LongestSubstringWithAtLeastKRepeatingCharacters {
     public int longestSubstring(String s, int k) {
-        if (k > s.length())
+        return longestSubstring(s, k, 0, s.length());
+    }
+
+    private int longestSubstring(String s, int k, int start, int end) {
+        int len = end - start;
+        if (k > len)
             return 0;
-        BadCharsTrackingMap map = new BadCharsTrackingMap(s, k);
-        SlidingWindow sw = new SlidingWindow(s, map, k);
-        return sw.findLongestString();
+        MapTrackingInvalidChars map = new MapTrackingInvalidChars(s, k, start, end);
+        if (map.allValid())
+            return len;
+        List<Interval> intervals = map.findValidIntervals();
+        int max = 0;
+        for (Interval interval : intervals) {
+            assert interval.start > start || interval.end < end;
+            max = Math.max(max, longestSubstring(s, k, interval.start, interval.end));
+        }
+        return max;
     }
     
-    private static class BadCharsTrackingMap {
-        private final Map<Character, Integer> counts = new HashMap<>();
-        private final Set<Character> badChars = new HashSet<>();
+    private static class MapTrackingInvalidChars {
+        private final String s;
         private final int k;
+        private final int start;
+        private final int end;
+        private final Map<Character, Integer> counts = new HashMap<>();
+        private final Set<Character> invalidChars = new HashSet<>();
 
-        BadCharsTrackingMap(String s, int k) {
+        MapTrackingInvalidChars(String s, int k, int start, int end) {
+            this.s = s;
             this.k = k;
-            for (int i = 0; i < s.length(); ++i)
+            this.start = start;
+            this.end = end;
+            for (int i = start; i < end; ++i)
                 add(s.charAt(i));
         }
         
         private void add(char c) {
             int count = counts.getOrDefault(c, 0) + 1;
             counts.put(c, count);
-            updateBadStatus(c, count);
+            updateValidity(c, count);
         }
 
-        private void updateBadStatus(char c, int count) {
-            if (isBad(count))
-                badChars.add(c);
+        private void updateValidity(char c, int count) {
+            if (isValidCount(count))
+                invalidChars.add(c);
             else
-                badChars.remove(c);
+                invalidChars.remove(c);
         }
 
-        private boolean isBad(int count) {
+        private boolean isValidCount(int count) {
             return count > 0 && count < k;
         }
         
@@ -49,97 +67,44 @@ public class LongestSubstringWithAtLeastKRepeatingCharacters {
                 counts.put(c, count);
             else
                 counts.remove(c);
-            updateBadStatus(c, count);
+            updateValidity(c, count);
         }
         
-        boolean allGood() {
-            return badChars.isEmpty();
+        boolean allValid() {
+            return invalidChars.isEmpty();
+        }
+
+        private List<Interval> findValidIntervals() {
+            List<Interval> intervals = new ArrayList<>();
+            for (int i = start; i < end; ) {
+                while (i < end && isInvalidChar(s.charAt(i)))
+                    ++i;
+                int j = i + 1;
+                while (j < end && isValidChar(s.charAt(j)))
+                    ++j;
+                if (i < end)
+                    intervals.add(new Interval(i, j));
+                i = j + 1;
+            }
+            return intervals;
+        }
+
+        private boolean isInvalidChar(char c) {
+            return invalidChars.contains(c);
+        }
+
+        private boolean isValidChar(char c) {
+            return !isInvalidChar(c);
         }
     }
+    
+    private static class Interval {
+        final int start;
+        final int end;
 
-    private static class SlidingWindow {
-
-        private final String string;
-        private final BadCharsTrackingMap map;
-        private int begin;
-        private int end;
-        private boolean leftToRight;
-        private final int k;
-
-        SlidingWindow(String string, BadCharsTrackingMap map, int k) {
-            this.string = string;
-            this.map = map;
-            this.k = k;
-            begin = 0;
-            end = string.length();
-        }
-
-        private int findLongestString() {
-            if (map.allGood())
-                return string.length();
-            for (int len = string.length() - 1; len >= k; --len) {
-                setDirection();
-                decreaseWindowSize();
-                if (map.allGood())
-                    return len;
-                while (!reachedOtherEnd()) {
-                    move();
-                    if (map.allGood())
-                        return len;
-                }
-            }
-            return 0;
-        }
-
-        private void setDirection() {
-            leftToRight = begin == 0;
-        }
-
-        private void decreaseWindowSize() {
-            if (leftToRight)
-                removeLastCharacter();
-            else
-                removeFirstCharacter();
-        }
-
-        private void removeLastCharacter() {
-            map.remove(string.charAt(--end));
-        }
-
-        private void removeFirstCharacter() {
-            map.remove(string.charAt(begin++));
-        }
-
-        private boolean reachedOtherEnd() {
-            if (leftToRight)
-                return end == string.length();
-            else
-                return begin == 0;
-        }
-
-        private void move() {
-            if (leftToRight)
-                moveRight();
-            else
-                moveLeft();
-        }
-
-        private void moveLeft() {
-            addFirstCharacter();
-            removeLastCharacter();
-        }
-
-        private void moveRight() {
-            removeFirstCharacter();
-            addLastCharacter();
-        }
-
-        private void addFirstCharacter() {
-            map.add(string.charAt(--begin));
-        }
-
-        private void addLastCharacter() {
-            map.add(string.charAt(end++));
+        public Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
         }
     }
 }
