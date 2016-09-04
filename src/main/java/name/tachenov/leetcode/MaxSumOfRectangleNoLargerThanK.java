cdb3992463853e5696f2a1648f2dc1d4c1c90f8a
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-04 08:39:00 +0300

    Merge Sort solution

--- src/main/java/name/tachenov/leetcode/MaxSumOfRectangleNoLargerThanK.java
+++ src/main/java/name/tachenov/leetcode/MaxSumOfRectangleNoLargerThanK.java
@@ -4,53 +4,87 @@
  */
 package name.tachenov.leetcode;
 
-import java.util.*;
-
 public class MaxSumOfRectangleNoLargerThanK {
     public int maxSumSubmatrix(int[][] matrix, int k) {
+        final int m = matrix.length;
+        final int n = matrix[0].length;
         RangeSum2D rangeSums = new RangeSum2D(matrix);
         int max = Integer.MIN_VALUE;
-        for (int width = 1; width <= matrix[0].length; ++width) {
-            for (int j = 0; j <= matrix[0].length - width; ++j) {
-                NavigableSet<Integer> minusSums = new TreeSet<>();
-                minusSums.add(0);
-                for (int i = 0; i < matrix.length; ++i) {
-                    int sum = rangeSums.rectangleSum(0, j, i + 1, width);
-                    // sum + minusSum should be max, but <= k
-                    Integer minusSum = minusSums.floor(k - sum);
-                    if (minusSum != null) {
-                        max = Math.max(max, minusSum + sum);
-                        if (max == k)
-                            return k;
-                    }
-                    minusSums.add(-sum);
-                }
+        for (int width = 1; width <= n; ++width) {
+            for (int j = 0; j <= n - width; ++j) {
+                long[] verticalPrefixSum = new long[m + 1];
+                for (int height = 0; height <= m; ++height)
+                    verticalPrefixSum[height] = rangeSums.rectangleSum(0, j, height, width);
+                max = Math.max(max, (int) mergeSort(verticalPrefixSum, k));
+                if (max == k)
+                    return k;
             }
         }
         return max;
     }
+
+    private static long mergeSort(long[] prefixSums, int k) {
+        return mergeSort(prefixSums, 0, prefixSums.length, k);
+    }
+
+    private static long mergeSort(long[] prefixSums, int start, int end, int k) {
+        if (end - start <= 1)
+            return Integer.MIN_VALUE;
+        int mid = (start + end) >>> 1;
+        long max = Math.max(mergeSort(prefixSums, start, mid, k),
+                            mergeSort(prefixSums, mid, end, k));
+        for (int left = start, right = mid + 1; left < mid; ++left) {
+            while (right < end && prefixSums[right] - prefixSums[left] <= k)
+                ++right;
+            long rangeSum = prefixSums[right - 1] - prefixSums[left];
+            if (rangeSum <= k)
+                max = Math.max(max, rangeSum);
+            if (max == k)
+                return k;
+        }
+        merge(prefixSums, start, mid, end);
+        return max;
+    }
+
+    private static void merge(long[] prefixSums, int start, int mid, int end) {
+        long[] temp = new long[end - start];
+        int left = start, right = mid, out = 0;
+        while (left < mid && right < end) {
+            if (prefixSums[left] <= prefixSums[right])
+                temp[out++] = prefixSums[left++];
+            else
+                temp[out++] = prefixSums[right++];
+        }
+        if (left < end)
+            System.arraycopy(prefixSums, left, temp, out, mid - left);
+        if (right < end)
+            System.arraycopy(prefixSums, right, temp, out, end - right);
+        System.arraycopy(temp, 0, prefixSums, start, end - start);
+    }
     
     private static class RangeSum2D {
-        final int[][] submatrixSum;
+        final long[][] topLeftSubmatrixSum;
 
         RangeSum2D(int[][] matrix) {
-            this.submatrixSum = new int[matrix.length + 1][matrix[0].length + 1];
-            for (int i = 0; i < matrix.length; ++i) {
-                for (int j = 0; j < matrix[i].length; ++j) {
-                    submatrixSum[i + 1][j + 1]
-                            = submatrixSum[i][j + 1]
-                            + submatrixSum[i + 1][j]
+            final int m = matrix.length;
+            final int n = matrix[0].length;
+            this.topLeftSubmatrixSum = new long[m + 1][n + 1];
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    topLeftSubmatrixSum[i + 1][j + 1]
+                            = topLeftSubmatrixSum[i][j + 1]
+                            + topLeftSubmatrixSum[i + 1][j]
                             + matrix[i][j]
-                            - submatrixSum[i][j];
+                            - topLeftSubmatrixSum[i][j];
                 }
             }
         }
         
-        int rectangleSum(int row, int col, int height, int width) {
-            return submatrixSum[row + height][col + width]
-                 - submatrixSum[row][col + width]
-                 - submatrixSum[row + height][col]
-                 + submatrixSum[row][col];
+        long rectangleSum(int row, int col, int height, int width) {
+            return topLeftSubmatrixSum[row + height][col + width]
+                 - topLeftSubmatrixSum[row][col + width]
+                 - topLeftSubmatrixSum[row + height][col]
+                 + topLeftSubmatrixSum[row][col];
         }
     }
 }
