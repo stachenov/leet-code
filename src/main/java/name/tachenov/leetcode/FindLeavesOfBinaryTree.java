8d86cd4ad3e97d6e05f4a4ae75295e9932d294f0
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-04 14:13:57 +0300

    One pass solution

--- src/main/java/name/tachenov/leetcode/FindLeavesOfBinaryTree.java
+++ src/main/java/name/tachenov/leetcode/FindLeavesOfBinaryTree.java
@@ -9,27 +9,18 @@ import java.util.*;
 public class FindLeavesOfBinaryTree {
     public List<List<Integer>> findLeaves(TreeNode root) {
         List<List<Integer>> leaves = new ArrayList<>();
-        Map<TreeNode, Integer> depths = new HashMap<>();
-        int depth = findDepths(root, depths);
-        for (int i = 0; i <= depth; ++i)
-            leaves.add(new ArrayList<>());
-        findLeaves(root, depths, leaves);
+        findLeaves(root, leaves);
         return leaves;
     }
 
-    private static int findDepths(TreeNode root, Map<TreeNode, Integer> depths) {
+    private int findLeaves(TreeNode root, List<List<Integer>> leaves) {
         if (root == null)
             return -1;
-        int depth = 1 + Math.max(findDepths(root.left, depths), findDepths(root.right, depths));
-        depths.put(root, depth);
+        int depth = 1 + Math.max(findLeaves(root.left, leaves),
+                                 findLeaves(root.right, leaves));
+        while (depth >= leaves.size())
+            leaves.add(new ArrayList<>());
+        leaves.get(depth).add(root.val);
         return depth;
-    }
-
-    private void findLeaves(TreeNode root, Map<TreeNode, Integer> depths, List<List<Integer>> leaves) {
-        if (root == null)
-            return;
-        leaves.get(depths.get(root)).add(root.val);
-        findLeaves(root.left, depths, leaves);
-        findLeaves(root.right, depths, leaves);
     }
 }
