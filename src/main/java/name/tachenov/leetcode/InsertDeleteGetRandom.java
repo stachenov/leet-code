e93904a1009d973661619b573c6fddb536be3565
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-04 10:07:40 +0300

    381. Insert Delete Get Random -- duplicates allowed

--- src/main/java/name/tachenov/leetcode/InsertDeleteGetRandom.java
+++ src/main/java/name/tachenov/leetcode/InsertDeleteGetRandom.java
@@ -8,36 +8,68 @@ import java.util.*;
 
 public class InsertDeleteGetRandom {
     
-    private final List<Integer> values = new ArrayList<>();
-    private final Map<Integer, Integer> indexes = new HashMap<>();
+    private final List<Value> values = new ArrayList<>();
+    private final Map<Value, Integer> indexes = new HashMap<>();
+    private final Map<Integer, Integer> counts = new HashMap<>();
     private final Random random = new Random();
     
     public boolean insert(int val) {
-        Integer index = indexes.get(val);
-        if (index == null) {
-            values.add(val);
-            indexes.put(val, values.size() - 1);
-            return true;
-        } else {
-            return false;
-        }
+        int count = counts.getOrDefault(val, 0);
+        Value newValue = new Value(val, count);
+        counts.put(val, count + 1);
+        values.add(newValue);
+        indexes.put(newValue, values.size() - 1);
+        return count == 0;
     }
     
     public boolean remove(int val) {
-        Integer index = indexes.get(val);
-        if (index == null) {
+        int count = counts.getOrDefault(val, 0);
+        if (count == 0)
             return false;
-        } else {
-            int last = values.get(values.size() - 1);
-            values.set(index, last);
-            indexes.put(last, index);
-            values.remove(values.size() - 1);
-            indexes.remove(val);
-            return true;
-        }
+        Value value = new Value(val, count - 1);
+        Integer index = indexes.get(value);
+        Value last = values.get(values.size() - 1);
+        values.set(index, last);
+        indexes.put(last, index);
+        values.remove(values.size() - 1);
+        indexes.remove(value);
+        if (count == 1)
+            counts.remove(val);
+        else
+            counts.put(val, count - 1);
+        return true;
     }
     
     public int getRandom() {
-        return values.get(random.nextInt(values.size()));
+        return values.get(random.nextInt(values.size())).value;
+    }
+    
+    private static class Value {
+        private final int value;
+        private final int number;
+
+        public Value(int value, int number) {
+            this.value = value;
+            this.number = number;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 3;
+            hash = 13 * hash + this.value;
+            hash = 13 * hash + this.number;
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+            final Value other = (Value) obj;
+            return this.value == other.value
+                    && this.number == other.number;
+        }
     }
 }
--- src/test/java/name/tachenov/leetcode/InsertDeleteGetRandomTest.java
+++ src/test/java/name/tachenov/leetcode/InsertDeleteGetRandomTest.java
@@ -13,35 +13,43 @@ public class InsertDeleteGetRandomTest {
     
     @Test(dataProvider = "nums")
     public void test(int[] nums) {
-        Set<Integer> present = new HashSet<>();
+        Map<Integer, Integer> present = new HashMap<>();
+        int total = 0;
         InsertDeleteGetRandom idgr = new InsertDeleteGetRandom();
         for (int n : nums) {
             if (n > 0) {
                 boolean added = idgr.insert(n);
-                if (present.contains(n)) {
+                if (present.getOrDefault(n, 0) > 0) {
                     assertThat(added).isFalse();
                 } else {
                     assertThat(added).isTrue();
-                    present.add(n);
                 }
+                present.put(n, present.getOrDefault(n, 0) + 1);
+                ++total;
             } else {
                 boolean removed = idgr.remove(-n);
-                if (present.contains(-n)) {
+                if (present.getOrDefault(-n, 0) > 0) {
                     assertThat(removed).isTrue();
-                    present.remove(-n);
                 } else {
                     assertThat(removed).isFalse();
                 }
+                if (removed) {
+                    if (present.getOrDefault(-n, 0) == 1)
+                        present.remove(-n);
+                    else
+                        present.put(-n, present.getOrDefault(-n, 0) - 1);
+                    --total;
+                }
             }
             Map<Integer, Integer> freq = new HashMap<>();
-            int sampleSize = present.size() * 100000;
+            int sampleSize = total * 100000;
             for (int i = 0; i < sampleSize; ++i) {
                 int random = idgr.getRandom();
                 freq.put(random, freq.getOrDefault(random, 0) + 1);
             }
-            for (int m : present) {
+            for (int m : present.keySet()) {
                 assertThat((float) freq.getOrDefault(m, 0) / sampleSize)
-                        .isCloseTo(1.0f / present.size(), withPercentage(1));
+                        .isCloseTo((float) present.getOrDefault(m, 0) / total, withPercentage(1));
             }
         }
     }
@@ -55,6 +63,7 @@ public class InsertDeleteGetRandomTest {
             {new int[] {1, 2, -2}},
             {new int[] {1, 2, -1}},
             {new int[] {+1, -2, +2, -1, -2}},
+            {new int[] {+1, +1, +2}},
         };
     }
     
