9aa2ae0b1c2efa0cc0fe90b16f553447f4b8e247
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-04 18:18:55 +0300

    Cleaner code

--- src/main/java/name/tachenov/leetcode/LongestSubstringWithAtLeastKRepeatingCharacters.java
+++ src/main/java/name/tachenov/leetcode/LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -10,72 +10,136 @@ public class LongestSubstringWithAtLeastKRepeatingCharacters {
     public int longestSubstring(String s, int k) {
         if (k > s.length())
             return 0;
-        FreqMap fm = new FreqMap(k);
-        for (int i = 0; i < s.length(); ++i)
-            fm.add(s.charAt(i));
-        if (fm.qualifies())
-            return s.length();
-        int begin = 0, end = s.length();
-        for (int len = s.length() - 1; len > 0; --len) {
-            if (begin == 0) {
-                fm.remove(s.charAt(--end));
-            } else {
-                fm.remove(s.charAt(begin++));
-            }
-            if (fm.qualifies())
-                return len;
-            if (begin == 0) {
-                while (end < s.length()) {
-                    fm.remove(s.charAt(begin++));
-                    fm.add(s.charAt(end++));
-                    if (fm.qualifies())
-                        return len;
-                }
-            } else {
-                while (begin > 0) {
-                    fm.add(s.charAt(--begin));
-                    fm.remove(s.charAt(--end));
-                    if (fm.qualifies())
-                        return len;
-                }
-            }
-        }
-        return 0;
+        BadCharsTrackingMap map = new BadCharsTrackingMap(s, k);
+        SlidingWindow sw = new SlidingWindow(s, map, k);
+        return sw.findLongestString();
     }
     
-    private static class FreqMap {
-        private final Map<Character, Integer> freq = new HashMap<>();
-        private final Set<Character> bad = new HashSet<>();
+    private static class BadCharsTrackingMap {
+        private final Map<Character, Integer> counts = new HashMap<>();
+        private final Set<Character> badChars = new HashSet<>();
         private final int k;
 
-        FreqMap(int k) {
+        BadCharsTrackingMap(String s, int k) {
             this.k = k;
+            for (int i = 0; i < s.length(); ++i)
+                add(s.charAt(i));
         }
         
-        void add(char c) {
-            int count = freq.getOrDefault(c, 0) + 1;
-            freq.put(c, count);
-            updateBad(count, c);
+        private void add(char c) {
+            int count = counts.getOrDefault(c, 0) + 1;
+            counts.put(c, count);
+            updateBadStatus(c, count);
         }
 
-        private void updateBad(int count, char c) {
-            if (count > 0 && count < k)
-                bad.add(c);
+        private void updateBadStatus(char c, int count) {
+            if (isBad(count))
+                badChars.add(c);
             else
-                bad.remove(c);
+                badChars.remove(c);
+        }
+
+        private boolean isBad(int count) {
+            return count > 0 && count < k;
         }
         
         void remove(char c) {
-            int count = freq.getOrDefault(c, 0) - 1;
+            int count = counts.getOrDefault(c, 0) - 1;
             if (count > 0)
-                freq.put(c, count);
+                counts.put(c, count);
             else
-                freq.remove(c);
-            updateBad(count, c);
+                counts.remove(c);
+            updateBadStatus(c, count);
         }
         
-        boolean qualifies() {
-            return bad.isEmpty();
+        boolean allGood() {
+            return badChars.isEmpty();
+        }
+    }
+
+    private static class SlidingWindow {
+
+        private final String string;
+        private final BadCharsTrackingMap map;
+        private int begin;
+        private int end;
+        private boolean leftToRight;
+        private final int k;
+
+        SlidingWindow(String string, BadCharsTrackingMap map, int k) {
+            this.string = string;
+            this.map = map;
+            this.k = k;
+            begin = 0;
+            end = string.length();
+        }
+
+        private int findLongestString() {
+            if (map.allGood())
+                return string.length();
+            for (int len = string.length() - 1; len >= k; --len) {
+                setDirection();
+                decreaseWindowSize();
+                if (map.allGood())
+                    return len;
+                while (!reachedOtherEnd()) {
+                    move();
+                    if (map.allGood())
+                        return len;
+                }
+            }
+            return 0;
+        }
+
+        private void setDirection() {
+            leftToRight = begin == 0;
+        }
+
+        private void decreaseWindowSize() {
+            if (leftToRight)
+                removeLastCharacter();
+            else
+                removeFirstCharacter();
+        }
+
+        private void removeLastCharacter() {
+            map.remove(string.charAt(--end));
+        }
+
+        private void removeFirstCharacter() {
+            map.remove(string.charAt(begin++));
+        }
+
+        private boolean reachedOtherEnd() {
+            if (leftToRight)
+                return end == string.length();
+            else
+                return begin == 0;
+        }
+
+        private void move() {
+            if (leftToRight)
+                moveRight();
+            else
+                moveLeft();
+        }
+
+        private void moveLeft() {
+            addFirstCharacter();
+            removeLastCharacter();
+        }
+
+        private void moveRight() {
+            removeFirstCharacter();
+            addLastCharacter();
+        }
+
+        private void addFirstCharacter() {
+            map.add(string.charAt(--begin));
+        }
+
+        private void addLastCharacter() {
+            map.add(string.charAt(end++));
         }
     }
 }
