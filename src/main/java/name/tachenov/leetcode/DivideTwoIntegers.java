9e450eed48a8425a1dc995983d4f17b2fc11cf7f
Sergey A. Tachenov <sergey@tachenov.name>
2016-09-20 22:12:30 +0300

    Use negative numbers!

--- src/main/java/name/tachenov/leetcode/DivideTwoIntegers.java
+++ src/main/java/name/tachenov/leetcode/DivideTwoIntegers.java
@@ -8,6 +8,38 @@ public class DivideTwoIntegers {
     public int divide(int dividend, int divisor) {
         if (divisor == 0 || (dividend == Integer.MIN_VALUE && divisor == -1))
             return Integer.MAX_VALUE;
+        if (dividend == 0 || divisor == 1) // necessary for MIN_VALUE / 1
+            return dividend;
+        // use negative numbers to avoid overflow, original idea by @brubru777
+        if (dividend > 0)
+            return -divide(-dividend, divisor);
+        if (divisor > 0)
+            return -divide(dividend, -divisor);
+        int shiftedDivisor = divisor;
+        int shift = 0;
+        while ((shiftedDivisor << 1) < 0) {
+            ++shift;
+            shiftedDivisor <<= 1;
+        }
+        int quotient = 0;
+        int remainder = dividend;
+        while (shift >= 0) {
+            if (remainder <= shiftedDivisor) {
+                quotient |= 1 << shift;
+                remainder -= shiftedDivisor;
+            }
+            shiftedDivisor >>= 1;
+            --shift;
+        }
+        return quotient;
+    }
+
+    /**
+     * This is my older variant. Just for reference.
+     */
+    public int divide_nonnegative(int dividend, int divisor) {
+        if (divisor == 0 || (dividend == Integer.MIN_VALUE && divisor == -1))
+            return Integer.MAX_VALUE;
         if (divisor == Integer.MIN_VALUE)
             return dividend == Integer.MIN_VALUE ? 1 : 0;
         if (divisor == 1) // needed for Integer.MIN_VALUE / 1
